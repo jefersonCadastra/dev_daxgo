f40679bf542919005c0857a73422701aa0860e31
Jeferson <jefferson.aurelio@cadastra.com>
2022-07-15 15:35:47 -0300

    Correção do nome da classe VisitOriginService

--- app/Http/Controllers/Painel/WizardController.php
+++ app/Http/Controllers/Painel/WizardController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Painel;
 use App\Http\Controllers\Controller;
 use App\Services\{
     WizardService,
-    visitOriginService,
+    VisitOriginService,
     behaviorDateService
 };
 use Illuminate\Http\Request;
