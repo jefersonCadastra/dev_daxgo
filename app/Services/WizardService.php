e9cfbe57f9fad02193dea16a1c37db775eae069e
Jeferson <jefferson.aurelio@cadastra.com>
2022-07-15 11:51:00 -0300

    Ajuste para o wizardService

--- app/Services/WizardService.php
+++ app/Services/WizardService.php
@@ -2,8 +2,26 @@
 
 namespace App\Services;
 
+use App\Models\{
+    Goal,
+    Visit,
+    VisitDetail
+};
+
 class WizardService
 {
+
+    private Goal $modelGoal;
+    private Visit $modelVisit;
+    private VisitDetail $modelVisitDetail;
+
+    public function __construct(Goal $modelGoal, Visit $modelVisit, VisitDetail $modelVisitDetail)
+    {
+        $this->modelGoal = $modelGoal;
+        $this->modelVisit = $modelVisit;
+        $this->modelVisitDetail = $modelVisitDetail;
+    }
+
     /**
      * Put data request in session "wizard"
      *
@@ -29,10 +47,90 @@ class WizardService
     public function storeFromSession()
     {
         // Retorna os dados e deleta a sessão
-        $data = session()->pull('wizard');
+        //$dataSession = session()->pull('wizard');
+        $dataSession = session('wizard');
+
+        //Invoicing Goal Create
+        $data = [];
+        $data['title'] = 'invoicing';
+        $data['month'] = $dataSession['month'];
+        $data['year'] = $dataSession['year'];
+        $data['value'] = $dataSession['invoicing'];
+        $this->modelGoal->create($data);
+
+        // //Approval Goal Create
+        $data = [];
+        $data['title'] = 'approval';
+        $data['month'] = $dataSession['month'];
+        $data['year'] = $dataSession['year'];
+        $data['value'] = $dataSession['approval'];
+        $this->modelGoal->create($data);
+
+        // //Ticket Goal Create
+        $data = [];
+        $data['title'] = 'ticket';
+        $data['month'] = $dataSession['month'];
+        $data['year'] = $dataSession['year'];
+        $data['value'] = $dataSession['ticket'];
+        $this->modelGoal->create($data);
+
+        // //Conversion Goal Create
+        $data = [];
+        $data['title'] = 'conversion';
+        $data['month'] = $dataSession['month'];
+        $data['year'] = $dataSession['year'];
+        $data['value'] = $dataSession['conversion'];
+        $this->modelGoal->create($data);
+
+        //Visits Create
+        $data = [];
+        $data['quantity'] = $dataSession['visits'];
+        $data['month'] = $dataSession['month'];
+        $data['year'] = $dataSession['year'];
+        $createdVisit = $this->modelVisit->create($data);
+        $idVisit = $createdVisit->id;
+
+        foreach ($dataSession as $key => $value) {
+
+            if (strpos($key, 'noPaidVisit_') !== false) {
+                $idVisitOrigin = explode('noPaidVisit_', $key);
+                $idVisitOrigin = $idVisitOrigin[1];
+                $qntVisitOrigin = $value;
+
+                if (empty($value))
+                    continue;
+
+                //Visit Detail Create
+                $data = [];
+                $data['visit_id'] = $idVisit;
+                $data['quantity'] = $qntVisitOrigin;
+                $data['origin'] = $idVisitOrigin;
+
+                $this->modelVisitDetail->create($data);
+            }
+
+            if (strpos($key, 'paidVisit_') !== false) {
+                $idVisitOrigin = explode('paidVisit_', $key);
+                $idVisitOrigin = $idVisitOrigin[1];
+                $qntVisitOrigin = $value;
+                $cpcVisitOrigin = $dataSession["paidVisitCpc_" . $idVisitOrigin];
+
+                if (empty($value))
+                    continue;
+
+                //Visit Detail Create
+                $data = [];
+                $data['visit_id'] = $idVisit;
+                $data['quantity'] = $qntVisitOrigin;
+                $data['origin'] = $idVisitOrigin;
+                $data['cpc'] = $cpcVisitOrigin;
+                $this->modelVisitDetail->create($data);
+            }
+        }
 
         // Chamar os services que irão persistir os dados
+        //$dataSession = session()->forget('wizard');
 
-        var_dump($data);
+        dd($dataSession);
     }
 }
